a813f9c2c950f5e6c3cce92d10b9405bc6436bb4
Huy Nguyen <alanriven@gmail.com>
2025-06-09 07:46:09 +0700

    config db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -11,6 +11,11 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 
 app.use("/", webRoutes);
+
+// test connection
+connection.query("select * from users u", function (err, results, fields) {
+  console.log("ket qua:", results);
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
